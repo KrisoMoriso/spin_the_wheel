7991c089025a2254ca6a202c874f09dff225f390
Kris J <104223005+KrisoMoriso@users.noreply.github.com>
2026-02-12 21:42:57 +0100

    szybko

--- spin_the_wheel.js
+++ spin_the_wheel.js
@@ -36,13 +36,13 @@ var list_2 = [
     "Krzysztof Z"
 ]
 browser.storage.local.get("excluded_names").then(x =>{
-    if (x == null){
+    if (x == undefined){
         browser.storage.local.set({excluded_names:[]})
     }
 })
 
 browser.storage.local.get("list_number").then(x =>{
-    if (x == null){
+    if (x == undefined){
         browser.storage.local.set({list_number: 3})
     }
 })
